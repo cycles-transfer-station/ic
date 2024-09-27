480b54b462a8d6e52e15d03e629d57c9e592d33b
Levi <levifeldman37@gmail.com>
2024-09-27 04:33:35 +0000

    second

--- rs/rosetta-api/icrc1/index-ng/src/main.rs
+++ rs/rosetta-api/icrc1/index-ng/src/main.rs
@@ -6,11 +6,11 @@ use ic_cdk::trap;
 use ic_cdk_macros::{init, post_upgrade, query};
 use ic_cdk_timers::TimerId;
 use ic_crypto_sha2::Sha256;
-use ic_icrc1::blocks::generic_block_to_encoded_block;
+use ic_icrc1::blocks::{encoded_block_to_generic_block, generic_block_to_encoded_block};
 use ic_icrc1::endpoints::StandardRecord;
 use ic_icrc1::{Block, Operation};
 use ic_icrc1_index_ng::{
-    GetAccountTransactionsArgs, GetAccountTransactionsResponse,
+    FeeCollectorRanges, GetAccountTransactionsArgs, GetAccountTransactionsResponse,
     GetAccountTransactionsResult, GetBlocksMethod, IndexArg, InitArg, ListSubaccountsArgs, Log,
     LogEntry, Status, TransactionWithId, UpgradeArg, DEFAULT_MAX_BLOCKS_PER_RESPONSE,
 };
@@ -931,9 +931,37 @@ fn account_block_ids_key(account: Account, block_index: BlockIndex64) -> Account
     (account_sha256(account), Reverse(block_index))
 }
 
+fn decode_icrc1_block(_txid: u64, bytes: Vec<u8>) -> GenericBlock {
+    let encoded_block = EncodedBlock::from(bytes);
+    encoded_block_to_generic_block(&encoded_block)
+}
 
 // Delete the get_blocks method. Clients must use the icrc3_get_blocks method on the ledger.
 
+#[query]
+#[candid_method(query)]
+fn get_blocks(req: GetBlocksRequest) -> ic_icrc1_index_ng::GetBlocksResponse {
+    let chain_length = with_blocks(|blocks| blocks.len());
+    let (start, length) = req
+        .as_start_and_length()
+        .unwrap_or_else(|msg| ic_cdk::api::trap(&msg));
+
+    let blocks = decode_block_range(start, length, decode_icrc1_block);
+    ic_icrc1_index_ng::GetBlocksResponse {
+        chain_length,
+        blocks,
+    }
+}
+
+fn decode_block_range<R>(start: u64, length: u64, decoder: impl Fn(u64, Vec<u8>) -> R) -> Vec<R> {
+    let length = length.min(with_state(|opts| opts.max_blocks_per_response));
+    with_blocks(|blocks| {
+        let limit = blocks.len().min(start.saturating_add(length));
+        (start..limit)
+            .map(|i| decoder(start + i, blocks.get(i).unwrap()))
+            .collect()
+    })
+}
 
 #[query]
 #[candid_method(query)]
@@ -1148,6 +1176,23 @@ pub fn encode_metrics(w: &mut ic_metrics_encoder::MetricsEncoder<Vec<u8>>) -> st
 }
 
 // Delete the get_fee_collectors_ranges method bc the CTS-CYCLES-BANK burns it's fees.
+
+#[candid_method(query)]
+#[query]
+fn get_fee_collectors_ranges() -> FeeCollectorRanges {
+    let ranges = with_state(|s| {
+        let mut res = vec![];
+        for (fee_collector, ranges) in &s.fee_collectors {
+            let mut fee_collector_ranges = vec![];
+            for range in ranges {
+                fee_collector_ranges.push((range.start.into(), range.end.into()));
+            }
+            res.push((*fee_collector, fee_collector_ranges))
+        }
+        res
+    });
+    FeeCollectorRanges { ranges }
+}
 
 fn main() {}
 
